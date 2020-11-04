812a6f1718e8a1271b3a91b11d61f69a98eae24d
0xdbe <0xdbe@mail.com>
2020-11-04 01:34:28 +0100

    update to azurerm 2.34

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 locals {
   azure_location          = "eastus"
   application_full_name   = "Hello World"
@@ -14,7 +18,7 @@ module "azure_ad_sp" {
 }
 
 module "azure_ressource_group" {
-  source                 = "git::https://github.com/0xdbe-terraform/terraform-azure-resource-group.git?ref=v2.0.0"
+  source                 = "git::https://github.com/0xdbe-terraform/terraform-azure-resource-group.git?ref=v2.0.1"
   azure_location          = local.azure_location
   application_full_name   = local.application_full_name
   application_short_name  = local.application_short_name
--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=2.32.0"
+      version = "=2.34.0"
     }
   }
   required_version = ">= 0.13"
