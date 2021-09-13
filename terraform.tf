ac7a856c8e5807d2f176f248f7fd8977b4a53829
0xdbe <23268141+0xdbe@users.noreply.github.com>
2021-09-13 23:22:53 +0200

    update azurerm provider

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=2.34.0"
+      version = ">=2.76.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
 }
